efec4e9ac3ebfe491fb306c6a327e9390ab5d578
netanelzula <netanelzula@gmail.com>
2026-09-10 21:15:28 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.zulatech.co.il',
+  trailingSlash: 'never',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
